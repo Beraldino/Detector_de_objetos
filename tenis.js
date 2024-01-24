24d7d3f29e8bfde0c53cb1ec1df82357d69eea89
Beraldino <150296949+Beraldino@users.noreply.github.com>
2024-01-24 20:33:04 -0300

    Add files via upload

---
+++ tenis.js
@@ -0,0 +1,43 @@
+img = "";
+objects = [];
+Status = "";
+
+function setup() {
+  canvas = createCanvas(640,420);
+  canvas.center();
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML = "Status: Detectando Objetos";
+}
+function preload() {
+    img = loadImage('tenis.webp');
+}
+function modelLoaded()
+{
+    console.log("Model Loaded!")
+    Status = true;
+    objectDetector.detect(img, gotResult);
+}
+function draw() {
+    image(img, 0, 0, 640, 420);
+
+    if(Status != "")
+    {
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status: Objeto Detectado";
+            console.log("olá");
+            fill("#FF0000");
+            porcent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + porcent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
